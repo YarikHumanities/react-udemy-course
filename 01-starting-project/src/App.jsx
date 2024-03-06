d595034720088d391ecd563765b7c1b50f83d540
Yarik <yaroslav.matsapura@gmail.com>
2024-03-06 23:38:15 +0200

    feat: use spread operator to populate component props with an object

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -40,24 +40,16 @@ function App() {
         <h2>Core Concepts</h2>
         <ul>
           <CoreConcept 
-            title={CORE_CONCEPTS[0].title}
-            description={CORE_CONCEPTS[0].description}
-            image={CORE_CONCEPTS[0].image}
+            {...CORE_CONCEPTS[0]}
           />
           <CoreConcept 
-            title={CORE_CONCEPTS[1].title}
-            description={CORE_CONCEPTS[1].description}
-            image={CORE_CONCEPTS[1].image}
+            {...CORE_CONCEPTS[1]}
           />
           <CoreConcept 
-            title={CORE_CONCEPTS[2].title}
-            description={CORE_CONCEPTS[2].description}
-            image={CORE_CONCEPTS[2].image}
+            {...CORE_CONCEPTS[2]}
           />
           <CoreConcept 
-            title={CORE_CONCEPTS[3].title}
-            description={CORE_CONCEPTS[3].description}
-            image={CORE_CONCEPTS[3].image}
+            {...CORE_CONCEPTS[3]}
           />
         </ul>
         </section>
