ba2458a1784813cef22ffdad796c7ad3af36cb6f
Yarik <yaroslav.matsapura@gmail.com>
2024-03-10 00:36:19 +0200

    refactor: derive Winner method from App component

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -12,6 +12,27 @@ const initialGameBoard = [
   [null, null, null],
 ];
 
+function derivedWinner(gameBoard, players) {
+  let winner;
+
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSquareSymbol =
+      gameBoard[combination[0].row][combination[0].column];
+    const secondSquareSymbol =
+      gameBoard[combination[1].row][combination[1].column];
+    const thirdSquareSymbol =
+      gameBoard[combination[2].row][combination[2].column];
+
+    if (
+      firstSquareSymbol &&
+      firstSquareSymbol === secondSquareSymbol &&
+      firstSquareSymbol === thirdSquareSymbol
+    ) {
+      winner = players[firstSquareSymbol];
+    }
+  }
+  return winner;
+}
 function deriveActivePlayer(gameTurns) {
   let currentPlayer = "X";
 
@@ -53,24 +74,7 @@ function App() {
     gameBoard[row][col] = player;
   }
 
-  let winner;
-
-  for (const combination of WINNING_COMBINATIONS) {
-    const firstSquareSymbol =
-      gameBoard[combination[0].row][combination[0].column];
-    const secondSquareSymbol =
-      gameBoard[combination[1].row][combination[1].column];
-    const thirdSquareSymbol =
-      gameBoard[combination[2].row][combination[2].column];
-
-    if (
-      firstSquareSymbol &&
-      firstSquareSymbol === secondSquareSymbol &&
-      firstSquareSymbol === thirdSquareSymbol
-    ) {
-      winner = players[firstSquareSymbol];
-    }
-  }
+  const winner = derivedWinner(gameBoard, players);
 
   const hasDraw = gameTurns.length === 9 && !winner;
 
