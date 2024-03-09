1c5e03a74acd017a74fea57dfc7a19a7b866e797
Yarik <yaroslav.matsapura@gmail.com>
2024-03-10 00:19:45 +0200

    feat: add Restart functionality

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -27,9 +27,13 @@ function App() {
   // const [hasWinner, setHasWinner] = useState(false);
   // const [activePlayer, setActivePlayer] = useState('X');
 
+  function handleRestart() {
+    setGameTurns([]);
+  }
+
   const activePlayer = deriveActivePlayer(gameTurns);
 
-  let gameBoard = initialGameBoard;
+  let gameBoard = [...initialGameBoard].map((array) => [...array]);
 
   for (const turn of gameTurns) {
     const { square, player } = turn;
@@ -88,7 +92,9 @@ function App() {
             isActive={activePlayer === "O"}
           />
         </ol>
-        {(winner || hasDraw) && <GameOver winner={winner} />}
+        {(winner || hasDraw) && (
+          <GameOver winner={winner} onRestart={handleRestart} />
+        )}
         <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
