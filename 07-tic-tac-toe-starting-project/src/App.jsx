05d40f7db5548d8edbc5e8d8d476766953fa07d5
Yarik <yaroslav.matsapura@gmail.com>
2024-03-09 13:51:53 +0200

    feat: add tic-tac-toe project and its starting menu functioanlity using two-way binding

---
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -0,0 +1,17 @@
+import Player from "./components/Player";
+
+function App() {
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player initialName="Player 1" symbol="X" />
+          <Player initialName="Player 2" symbol="0" />
+        </ol>
+        CAME BOARD
+      </div>
+    </main>
+  );
+}
+
+export default App;
---
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(initialName);
+
+  function editButtonClickHandling() {
+    //When you call setIsEditing and pass a function to it,
+    //React internally passes the CURRENT state value as
+    //an argument to that function. !!!CURRENT BY THE MOMENT OF SCHEDULED EXECUTION OF STATE CHANGING
+    setIsEditing((editing) => !editing);
+  }
+
+  function handleChange(event) {
+    console.log(event);
+    setPlayerName(event.target.value);
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {!isEditing ? (
+          <span className="player-name">{playerName}</span>
+        ) : (
+          <input
+            type="text"
+            required
+            value={playerName}
+            onChange={handleChange}
+          ></input>
+        )}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={editButtonClickHandling}>
+        {isEditing ? "Save" : "Edit"}
+      </button>
+    </li>
+  );
+}
