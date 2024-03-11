bbc8d0e25a699f054cc2e855bf2cd0adbbd7f4c1
Yarik <yaroslav.matsapura@gmail.com>
2024-03-11 17:16:54 +0200

    feat: add name input functionality using React Core Concepts just

--- Refs-and-portals-Game/src/components/Player.jsx
+++ Refs-and-portals-Game/src/components/Player.jsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
+
 export default function Player() {
+  const [playerName, setPlayerName] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+
+  function handleClick() {
+    setSubmitted(true);
+  }
+
+  function handleChange(event) {
+    setSubmitted(false);
+    setPlayerName(event.target.value);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submitted ? playerName : "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleChange} value={playerName} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
