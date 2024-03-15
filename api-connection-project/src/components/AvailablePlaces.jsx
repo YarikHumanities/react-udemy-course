482c055dc0d31b34a30c6582baddaee2c8a4a7da
Yarik <yaroslav.matsapura@gmail.com>
2024-03-15 12:44:28 +0200

    refactor: use async await now to fetch places data from the server

--- api-connection-project/src/components/AvailablePlaces.jsx
+++ api-connection-project/src/components/AvailablePlaces.jsx
@@ -5,13 +5,13 @@ export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:3000/places")
-      .then((response) => {
-        return response.json();
-      })
-      .then((resData) => {
-        setAvailablePlaces(resData.places);
-      });
+    async function fetchPlaces() {
+      const responce = await fetch("http://localhost:3000/places");
+      const resData = await responce.json();
+      setAvailablePlaces(resData.places);
+    }
+
+    fetchPlaces();
   }, []);
   return (
     <Places
