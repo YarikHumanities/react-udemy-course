e9cd2911d4699984e97c74b584eb42aaf119449c
Yarik <yaroslav.matsapura@gmail.com>
2024-03-15 15:41:26 +0200

    faet: add PUT fetch to server using Optimistic Updating Pattern

--- api-connection-project/src/http.js
+++ api-connection-project/src/http.js
@@ -9,3 +9,18 @@ export async function fetchAvailablePlaces() {
 
     return resData.places;
 }
+
+export async function updateUserPlaces(places) {
+    const responce = await fetch('http://localhost:3000/user-places', {
+        method: "PUT",
+        body: JSON.stringify({ places }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    const resData = await responce.json();
+    if (!responce.ok) {
+        throw new Error("Failed to update user data.")
+    }
+    return resData.message;
+}
