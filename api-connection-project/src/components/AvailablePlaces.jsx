c47dd301e6469c57cfc63235564c740d6978f583
Yarik <yaroslav.matsapura@gmail.com>
2024-03-15 14:05:03 +0200

    feat: add handling of loading while fetching

--- api-connection-project/src/components/AvailablePlaces.jsx
+++ api-connection-project/src/components/AvailablePlaces.jsx
@@ -3,12 +3,15 @@ import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [isFetching, setIsFetching] = useState(true);
 
   useEffect(() => {
     async function fetchPlaces() {
+      setIsFetching(true);
       const responce = await fetch("http://localhost:3000/places");
       const resData = await responce.json();
       setAvailablePlaces(resData.places);
+      setIsFetching(false);
     }
 
     fetchPlaces();
@@ -17,6 +20,8 @@ export default function AvailablePlaces({ onSelectPlace }) {
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching Data ..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
